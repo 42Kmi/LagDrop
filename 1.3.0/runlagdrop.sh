bd7a9326a96fcf41bc813c44daa6beb5e71fce08
42Kmi <torengo@42kmi.com>
2016-06-20 21:26:13 -0400

    Changes
    minor

--- 1.3.0/runlagdrop.sh
+++ 1.3.0/runlagdrop.sh
@@ -6,8 +6,8 @@ DIR=$(echo $0 | sed -E "s/\/$SCRIPTNAME//g")
 ##### Add Lagdrop Scripts Here #####
 #{ until $DIR/lagdrop_wiiu.sh; do eval "$DIR/lagdrop_wiiu.sh"; done && until $DIR/lagdrop_3ds.sh; do eval "$DIR/lagdrop_3ds.sh"; done && until $DIR/lagdrop_xbox.sh; do eval "$DIR/lagdrop_xbox.sh"; done; } &> /dev/null LAGDROP1=$!
 {{ until $DIR/lagdrop_wiiu.sh; do eval "$DIR/lagdrop_wiiu.sh"; done; } &> /dev/null LAGDROP1=$!; } &
-{{ until $DIR/lagdrop_3ds.sh; do eval "$DIR/lagdrop_3ds.sh"; done; } &> /dev/null LAGDROP2=$!; } &
-{{ until $DIR/lagdrop_xbox.sh; do eval "$DIR/lagdrop_xbox.sh"; done; } &> /dev/null LAGDROP3=$!; } &
+&& {{ until $DIR/lagdrop_3ds.sh; do eval "$DIR/lagdrop_3ds.sh"; done; } &> /dev/null LAGDROP2=$!; } &
+&& {{ until $DIR/lagdrop_xbox.sh; do eval "$DIR/lagdrop_xbox.sh"; done; } &> /dev/null LAGDROP3=$!; } &
 ##### Add Lagdrop Scripts Here #####
 wait
 ##### Run LagDrop On Startup #####
