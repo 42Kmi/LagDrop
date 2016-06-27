4c196ec3f0154c24543d053ccda72960ce266637
42Kmi <torengo@42kmi.com>
2016-06-27 02:43:44 -0400

    v1.5.0

--- LagDrop/lagdrop_wiiu.sh
+++ LagDrop/lagdrop_wiiu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##### 42Kmi LagDrop, Written by 42Kmi. Property of 42Kmi, LLC. #####
-##### Ver 1.4.1
+##### Ver 1.5.0
 ######################################################################################################
 #               .////////////   -+osyyys+-   `////////////////////-                      `//////////`#
 #              /Ny++++++++hM+/hNho/----:+hNo hN++++++oMMm++++++mMy`                      hMhhhhhhdMh #
@@ -25,7 +25,9 @@
 ##### Be Glorious #####
 ##### Ban SLOW Peers #####
 
-
+##### Prepare LagDrop's IPTABLES Chains #####
+if { iptables -L| grep -Eoq "(LDREJECT|LDACCEPT).*anywhere"; }; then :; else iptables -N LDREJECT; iptables -P LDREJECT REJECT; iptables -N LDACCEPT; iptables -P LDACCEPT ACCEPT; iptables -t filter -A INPUT -j LDACCEPT; iptables -t filter -A INPUT -j LDREJECT; fi
+##### Prepare LagDrop's IPTABLES Chains #####
 
 ##### Make Files #####
 CONSOLENAME=wiiu
@@ -35,13 +37,12 @@ DIR=$(echo $0 | sed -E "s/\/$SCRIPTNAME//g")
 GETSTATIC=$(echo `nvram get static_leases | grep -E -i -o "$CONSOLENAME.*=([0-9]{1,3}.?){4}" | sed -E 's/=? .*//g' | grep -E -o "([0-9]{1,3}.?){4}"| sed -E 's/\=$//g'`)
 if [ ! -f $DIR/42Kmi ] ; then mkdir -p $DIR/42Kmi ; fi
 if [ ! -f $DIR/42Kmi/options_$CONSOLENAME.txt ] ; then echo -e "$CONSOLENAME=$GETSTATIC\nLIMIT=3\nCOUNT=5\nSIZE=1024\nCLEARINTERVAL=300\n;" > $DIR/42Kmi/options_$CONSOLENAME.txt; fi ### Makes options file if it doesn't exist
-#if [ ! -f $DIR/42Kmi/peerip_$CONSOLENAME.txt ]; then echo "0.0.0.0" > $DIR/42Kmi/peerip_$CONSOLENAME.txt; fi
 ##### Make Files #####
 CONSOLE=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/options_$CONSOLENAME.txt | sed -n 1p | sed -E 's/^.*=//g') ### Your Wii U's IP address. Change this in the $CONSOLENAMEip.txt file
 LIMIT=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/options_$CONSOLENAME.txt | sed -n 2p | sed -E 's/^.*=//g') ### Your max average millisecond limit. Peers pinging higher than this value are blocked. Default is 3.
 COUNT=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/options_$CONSOLENAME.txt | sed -n 3p | sed -E 's/^.*=//g') ### How many packets to send. Default is 5
 SIZE=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/options_$CONSOLENAME.txt | sed -n 4p | sed -E 's/^.*=//g') ### Size of packets. Default is 1024
-CLEARINTERVAL=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/options_$CONSOLENAME.txt | sed -5 4p | sed -E 's/^.*=//g') ### Size of packets. Default is 1024
+CLEARINTERVAL=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/options_$CONSOLENAME.txt | sed -5 4p | sed -E 's/^.*=//g') ### Clear time in seconds. Default is 300
 ROUTER=$(nvram get lan_ipaddr | grep -E -o '(([0-9]{1,3}\.?){4})')
 ROUTERSHORT=$(nvram get lan_ipaddr | grep -E -o '(([0-9]{1,3}\.?){2})' | sed -n 1p)
 WANSHORT=$(nvram get wan_ipaddr | grep -E -o '(([0-9]{1,3}\.?){2})' | sed -n 1p)
@@ -53,10 +54,8 @@ else
 EXTRAIP=$(while read -r i; do echo "${i%}"; done < /$DIR/42Kmi/extraip.txt | sed -n 1p ) ### Additional IPs to filter out. Make extraip.txt in 42Kmi folder, add IPs there. See README
 PEERIP=$(while read -r i; do echo "${i%}"; done < /proc/net/ip_conntrack | grep "${CONSOLE}" | grep -E -o '(([0-9]{1,3}\.?){4})' | grep -o '^.*\..*$' | grep -v "${CONSOLE}" | grep -v "${ROUTER}" | grep -E -v "^192\.168\.(([0-9]{1,3}\.?){2})" | grep -E -v "^$ROUTERSHORT" | grep -E -v "^$WANSHORT" | egrep -E -v "$FILTERIP" | egrep -E -v "$EXTRAIP" | awk '!a[$0]++' | sed -n 1p ) ### Get Wii U Peer's IP
 fi
-RESOLVE=$(nslookup $PEERIP | grep -Fq "Address 1: $PEERIP" | sed -E "s/^.*$PEERIP\ //g")
-mdev=$(ping -q -c "${COUNT}" -W 1 -s "${SIZE}" "${PEERIP}" | grep -F "round-trip" | sed -E 's/^.*([0-9]{1,9}\.[0-9]{3}\/){2}//g' | sed -E "s/\..*ms//g"; &> /dev/null) ### Get mdev from ping
-BLOCK=$({ if [ "${mdev}" -gt "${LIMIT}" ]; then if { iptables -L | grep -Fq "$PEERIP" ;} || { iptables -L | grep -q `echo "$PEERIP" | sed -E 's/\./-/g'`; } || { iptables -L | grep -Fq "$RESOLVE" ;} || { iptables -nL|sort -u|grep -Fq "$PEERIP"|sed 's/$PEERIP//g'|sed -E 's/^.*--//g'|grep -Eo "(([0-9]{0,3})\.?){4}"|sort -u|grep -Fq "$PEERIP" ; }; then :; else { eval "iptables -A INPUT -p all -s $CONSOLE -d $PEERIP -j REJECT" && eval "iptables -A INPUT -p all -s $PEERIP -d $CONSOLE -j REJECT" && eval "iptables -A OUTPUT -p all -s $CONSOLE -d $PEERIP -j REJECT" && eval "iptables -A OUTPUT -p all -s $PEERIP -d $CONSOLE -j REJECT"; } fi; else { eval "iptables -A INPUT -p all -s $CONSOLE -d $PEERIP -j ACCEPT" && eval "iptables -A INPUT -p all -s $PEERIP -d $CONSOLE -j ACCEPT" && eval "iptables -A OUTPUT -p all -s $CONSOLE -d $PEERIP -j ACCEPT" && eval "iptables -A OUTPUT -p all -s $PEERIP -d $CONSOLE -j ACCEPT"; } fi; }&) 
-KILLOLD=$(kill -9 `ps -w | grep -F "$SCRIPTNAME" | grep -v $$` &> /dev/null)
+mdev=$({ if { iptables -nL| grep -Foq "$PEERIP"; }; then :; else ping -q -c "${COUNT}" -W 1 -s "${SIZE}" "${PEERIP}" | grep -F "round-trip" | sed -E 's/^.*([0-9]{1,9}\.[0-9]{3}\/){2}//g' | sed -E "s/\..*ms//g"; &> /dev/null; fi; } &) ### Get mdev from ping
+BLOCK=$({ if { iptables -nL| grep -Foq "$PEERIP"; }; then :; else { if [ "${mdev}" -gt "${LIMIT}" ]; then { eval "iptables -A LDREJECT -p all -s $PEERIP -d $CONSOLE -j REJECT"; }; else { eval "iptables -A LDACCEPT -p all -s $PEERIP -d $CONSOLE -j ACCEPT"; } fi; } fi; } &)
 LOOP=$(exec "$0" && $KILLOLD && kill $$)
 
 {
@@ -80,7 +79,9 @@ rm -f ${LOCKFILE}
 } &
 
 ### Clear iptables ###
-{ { while sleep "$CLEARINTERVAL"; do { { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLENAME" | grep "ACCEPT" | grep -oE "(([0-9]{1,9})).*ACCEPT" | grep -oE "(([0-9]{1,9}))" | sort -u | sed -E 's/^/iptables -D INPUT /g' | sed -E 's/$/\n/g')"; } ; { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLENAME" | grep "ACCEPT" | grep -oE "(([0-9]{1,9})).*ACCEPT" | grep -oE "(([0-9]{1,9}))" | sort -u | sed -E 's/^/iptables -D OUTPUT /g' | sed -E 's/$/\n/g')"; } ; { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLENAME" | grep "REJECT" | grep -oE "(([0-9]{1,9})).*REJECT" | grep -oE "(([0-9]{1,9}))" | sort -u | sed -E 's/^/iptables -D INPUT /g' | sed -E 's/$/\n/g')"; } ; { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLENAME" | grep "REJECT" | grep -oE "(([0-9]{1,9})).*REJECT" | grep -oE "(([0-9]{1,9}))" | sort -u | sed -E 's/^/iptables -D OUTPUT /g' | sed -E 's/$/\n/g')"; }; }; done; } &> /dev/null; } &
+{ { while sleep "$CLEARINTERVAL"; do { { eval "echo $(iptables -nL --line-numbers | grep -i "$CONSOLE" | grep "ACCEPT" | grep -oE "(([0-9]{1,9})).*ACCEPT" | grep -oE "(([0-9]{1,9}))" | awk '!x[$0]++' | sed -E 's/^/iptables -D INPUT /g' | sed -E 's/$/\n/g')"; } ; { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLE" | grep "ACCEPT" | grep -oE "(([0-9]{1,9})).*ACCEPT" | grep -oE "(([0-9]{1,9}))" | awk '!x[$0]++' | sed -E 's/^/iptables -D OUTPUT /g' | sed -E 's/$/\n/g')"; } ; { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLE" | grep "REJECT" | grep -oE "(([0-9]{1,9})).*REJECT" | grep -oE "(([0-9]{1,9}))" | awk '!x[$0]++' | sed -E 's/^/iptables -D INPUT /g' | sed -E 's/$/\n/g')"; } ; { eval "echo $(iptables -L --line-numbers | grep -i "$CONSOLE" | grep "REJECT" | grep -oE "(([0-9]{1,9})).*REJECT" | grep -oE "(([0-9]{1,9}))" | awk '!x[$0]++' | sed -E 's/^/iptables -D OUTPUT /g' | sed -E 's/$/\n/g')"; }; }; done; } &> /dev/null; } &
+
+{ { while sleep "$CLEARINTERVAL"; do { iptables -F LDREJECT; iptables -F LDACCEPT;}; done; } &> /dev/null; } & 
 ### Clear iptables ###
 
 {
@@ -89,7 +90,7 @@ $KILLOLD
 { while ping -q -c 1 -W 1 "${CONSOLE}" | grep -q -F -w "100% packet loss"; do :; done ;} &> /dev/null; wait
 #while sleep 60; do 
 while sleep :; do 
- if { iptables -L | grep -Fq "$PEERIP" ;} || { iptables -L | grep -q `echo "$PEERIP" | sed -E 's/\./-/g'`; } || { iptables -L | grep -Fq "$RESOLVE" || { iptables -nL|sort -u|grep -Fq "$PEERIP"|sed 's/$PEERIP//g'|sed -E 's/^.*--//g'|grep -Eo "(([0-9]{0,3})\.?){4}"|sort -u|grep -Fq "$PEERIP" ; };}; then :; else ${BLOCK}; sleep $((2 * $COUNT)); wait &> /dev/null; fi
+ if { iptables -nL| grep -Foq "$PEERIP"; }; then :; else ${BLOCK}; sleep $((2 * $COUNT)); wait &> /dev/null; fi
 done
 fi
 
