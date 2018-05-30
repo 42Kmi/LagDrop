8ab2b54a1170c3fe8e6a84951671c8132318bcb4
42Kmi.com <torengo@42kmi.com>
2018-05-30 19:38:01 -0400

    Bugfix

--- LagDrop 3.0.0 beta/lagdropclear.sh
+++ LagDrop 3.0.0 beta/lagdropclear.sh
@@ -4,7 +4,7 @@ kill -9 $(ps -w | grep -F "$SCRIPTNAME" | grep -v $$) &> /dev/null
 
 ##### Find Shell #####
 SHELLIS=$(if [ -f "/usr/bin/lua" ]; then echo "ash"; else echo "no"; fi)
-WAITLOCK=$(if [ "${SHELLIS}" = "ash" ]; then "-w"; else :; fi)
+WAITLOCK=$(if [ "${SHELLIS}" = "ash" ]; then echo "-w"; else echo ""; fi)
 ##### Find Shell #####
 ##### LagDrop iptables clear script. Clears LagDrop related iptables#####
 ##### Add this to cron to run at some interval. Execute this script to clear the LagDrop chains from iptables
@@ -15,9 +15,9 @@ MODE=0 #0 = clears LDACCEPT and LDREJECT; 1 = clears LDREJECT only.
 
 ### Magic happens here ###
 if [ "${MODE}" != 1 ]; then
-iptables -F LDREJECT "${WAITLOCK}" && iptables -F LDACCEPT "${WAITLOCK}"
+iptables -F LDREJECT && iptables -F LDACCEPT
 else
-iptables -F LDACCEPT "${WAITLOCK}"
+iptables -F LDACCEPT 
 fi
 ### Magic happens here ###
 ##### 42Kmi LagDrop, Written by 42Kmi. Property of 42Kmi, LLC. #####
--- LagDrop 3.0.0 beta/lagdropcount.sh
+++ LagDrop 3.0.0 beta/lagdropcount.sh
@@ -5,13 +5,16 @@ kill -9 $(ps -w | grep -F "$SCRIPTNAME" | grep -v $$) &> /dev/null
 
 ##### Find Shell #####
 SHELLIS=$(if [ -f "/usr/bin/lua" ]; then echo "ash"; else echo "no"; fi)
-WAITLOCK=$(if [ "${SHELLIS}" = "ash" ]; then "-w"; else :; fi)
+WAITLOCK=$(if [ "${SHELLIS}" = "ash" ]; then echo "-w"; fi)
 ##### Find Shell #####
 
-ALLOWED=$(iptables -nL LDACCEPT "${WAITLOCK}"|grep -oE "^ACCEPT"| wc -l)
-BLOCKED=$(iptables -nL LDREJECT "${WAITLOCK}"|grep -oE "^(REJECT|DROP)"| wc -l)
-while :; do (printf "\ec"; echo "---------------------------"; echo "42Kmi LagDrop Count";(echo "Allowed peers: $(iptables -nL LDACCEPT "${WAITLOCK}"|grep -oE "^ACCEPT"| wc -l)" && echo "Blocked peers: $(iptables -nL LDREJECT "${WAITLOCK}"|grep -oE "^(REJECT|DROP)"| wc -l)"; sleep 5)); done
+#ALLOWED=$(iptables -nL LDACCEPT "${WAITLOCK}"|grep -oE "^ACCEPT"| wc -l)
+#BLOCKED=$(iptables -nL LDREJECT "${WAITLOCK}"|grep -oE "^(REJECT|DROP)"| wc -l)
 
-#while :; do (printf "\ec"; echo "---------------------------"; echo "42Kmi LagDrop Count";(echo "Allowed peers: "${ALLOWED}" ($(( ALLOWED * 100/(ALLOWED + BLOCKED + 1) ))%)" && echo "Blocked peers: "${BLOCKED}" ($(( BLOCKED * 100/(ALLOWED + BLOCKED + 1) ))%)"; sleep 5)); done
-
-#while :; do (printf "\ec"; echo "---------------------------"; echo "42Kmi LagDrop Count";(echo "Allowed peers: "${ALLOWED}" ($(( ( ALLOWED + 1) * 100/(ALLOWED + BLOCKED + 1) ))%)" && echo "Blocked peers: "${BLOCKED}" ($(( ( BLOCKED + 1 ) * 100/(ALLOWED + BLOCKED + 1) ))%)"; sleep 5)); done
+ALLOWED=$(iptables -nL LDACCEPT |grep -oE "^ACCEPT"| wc -l)
+BLOCKED=$(iptables -nL LDREJECT |grep -oE "^(REJECT|DROP)"| wc -l)
+while :;
+do (printf "\ec"; echo "---------------------------";
+echo -e "42Kmi LagDrop Count\nAllowed peers: "$ALLOWED" \nBlocked peers: "$BLOCKED"";
+sleep 5);
+done
